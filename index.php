f582a89be64140442e32274f1b67bda536443519
Baris Demirdelen <barisdemirdelen@gmail.com>
2015-04-27 21:16:05 +0300

    amazing css

--- index.php
+++ index.php
@@ -2,17 +2,25 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <script type="text/javascript" src="/bot/ajax.js"></script>
+    <script type="text/javascript" src="ajax.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Barış Demirdelen</title>
 </head>
 <body>
-Merhaba, ben Barış Demirdelen. Eğer çevirimiçiysem benimle bu kutucuktan konuşabilirsiniz. <b>Şu an çevirimiçiyim.</b>
-<br/>
-<input type="text" id="message" onkeypress="{if (event.keyCode==13)process()}"/>
-<br/>
+<section class="messages">
+    <p>Merhaba, ben Barış. Eğer çevirimiçiysem benimle bu kutucuktan konuşabilirsiniz. <b>Şu an
+            çevirimiçiyim.</b></p>
+    <br/>
 
-<div id="divMessage">
-    Barış: Naber?
-</div>
+    <input type="text" id="message" onkeypress="{if (event.keyCode==13)process()}"/>
+    <br/>
+    <br/>
+
+    <div id="divMessage">
+        <div class="leftBubble">
+            <p>Naber?</p>
+        </div>
+    </div>
+</section>
 </body>
 </html>
